ecb53f388a2262cfe74414eb9fd356fe56a1feab
tester-404 <59533456+tester-404@users.noreply.github.com>
2021-02-22 11:23:38 -0800

    Add files via upload

---
+++ uploadInputHidden.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+describe('Upload File Hidden Input', () => {
+    it('should be able to upload a file on hidden input', () => {
+    const inputDiv = $('#div_file_box0');
+    const input = $('#input_file0');
+    const submitBtn = $('.convert_button');
+    const filePath = path.join(__dirname, '../la.PNG');
+    const remoteFilePath = browser.uploadFile(filePath);
+
+    browser.url('https://online2pdf.com/');
+    browser.execute(function () {
+    document.getElementById('div_file_box0').style.display = 'block';
+    });
+
+    inputDiv.waitForDisplayed();
+
+    input.setValue(remoteFilePath);
+    submitBtn.click();
+    browser.pause(9000); // chai assertion
+
+    });
+    
+});
+
+// npx wdio wdio.conf.js
